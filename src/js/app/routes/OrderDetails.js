032c70f1cf50fb3468f7ebd7715d2c65935382a7
nayzi <ysn.hamzaoui@gmail.com>
2015-02-27 13:02:19 +0100

    order

--- src/js/app/routes/OrderDetails.js
+++ src/js/app/routes/OrderDetails.js
@@ -5,17 +5,7 @@
 
 App.OrderDetailsRoute = Ember.Route.extend(Ember.SimpleAuth.AuthenticatedRouteMixin, {
     
- model: function(params) {console.log('orderdetails.js model :'+ params.order_id);        var ord = this.get('store').find('order', params.order_id).then(fulfill, reject);
-
-        function fulfill(answer) {
-            console.log("The answer is " + answer.otp);
-
-            return answer;
-        }
-
-        function reject(reason) {
-            console.log("Couldn't get the answer! Reason: " + reason);
-        }
+ model: function(params) {return this.get('store').find('order', params.order_id);
 
 
 
