7dd91b16ab7538ca31d57e0c9952b63bb7d86cb4
nayzi <ysn.hamzaoui@gmail.com>
2015-03-05 13:27:58 +0100

    op

--- src/js/app/helpers/withh.js
+++ src/js/app/helpers/withh.js
@@ -1,7 +1,7 @@
 Ember.Handlebars.registerBoundHelper("withh", function(a,b) {
     var z = a.toArray();
   
-   for(var i=0;i<z.length;i++){b=b.replace("${"+z[i]._data.optionType._data.replace+"}", z[i]._data.opValue);}
+   for(var i=0;i<z.length;i++){b=b.replace("${"+z[i]._data.optionType._data.replace+"}", z[i]._data.optionValue);}
     return b;
    
     });
