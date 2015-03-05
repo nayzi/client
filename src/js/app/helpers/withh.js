b080f0359d7546506e6435c3a75d669dc03d8400
nayzi <ysn.hamzaoui@gmail.com>
2015-03-05 14:54:28 +0100

    withh

--- src/js/app/helpers/withh.js
+++ src/js/app/helpers/withh.js
@@ -1,7 +1,13 @@
 Ember.Handlebars.registerBoundHelper("withh", function(a,b) {
-    var z = a.toArray();
-  
-   for(var i=0;i<z.length;i++){b=b.replace("${"+z[i]._data.optionType._data.replace+"}", z[i]._data.optionValue);}
-    return b;
-   
+    for (var c = a.toArray(), d = 0; d < c.length; d++) {
+var rep =c[d]._data.optionType._data.replace;
+var val= c[d]._data.optionValue;
+var v="";
+if(rep=="L4"&&val.length!=null){
+for(var i=0;i<4-val.length;i++){v+="0";}
+val=v+val;
+}
+
+        console.log("variable : "+val+" remplace :"+rep); b = b.replace("${" + rep + "}", val);}
+    return b
     });
