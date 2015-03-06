aa6ae5a6eeb19a1f1a4c511ae46a177ce899f235
nayzi <ysn.hamzaoui@gmail.com>
2015-03-06 09:26:59 +0100

    nb pieces

--- src/js/app/helpers/withh.js
+++ src/js/app/helpers/withh.js
@@ -1,14 +1,20 @@
-Ember.Handlebars.registerBoundHelper("withh", function(a,b) {
+Ember.Handlebars.registerBoundHelper("withh", function(a, b) {
     for (var c = a.toArray(), d = 0; d < c.length; d++) {
-var rep =c[d]._data.optionType._data.replace;
-var val= c[d]._data.optionValue;
-var v="";
-if(rep=="L4" && (val!=null)){
-if(val.length!=null){	
-for(var i=0;i<4-val.length;i++){v+="0";}
-val=v+val;}
-}
+        var e = c[d]._data.optionType._data.replace,
+            f = c[d]._data.optionValue,
+            h = "";
+        if ("L4" == e && null != f && null != f.length) {
+            for (var g = 0; g < 4 - f.length; g++) h += "0";
+            f = h + f
+        }
+        console.log("variable : " + f + " remplace :" + e);
+        console.log("var :" + c[d]._data.option._data.value + " remplace :" + c[d]._data.option._data.label);
+        b = b.replace("${" + e + "}", f);
+        if ((b.indexOf("B") > -1) && ((c[d]._data.option._data.label == "Basse") || (c[d]._data.option._data.label == "Haute"))) {
+            var z = (c[d]._data.option._data.label == "Haute") ? 1 : 0;
+            b = b.replace("B", z);
+        }
 
-        console.log("variable : "+val+" remplace :"+rep); b = b.replace("${" + rep + "}", val);}
+    }
     return b
-    });
+});
