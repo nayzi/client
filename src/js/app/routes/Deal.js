2cc011fc3dfa55e4a2c3fc55586e901fcb29f8d8
nayzi <ysn.hamzaoui@gmail.com>
2015-03-11 11:48:05 +0100

    ref

--- src/js/app/routes/Deal.js
+++ src/js/app/routes/Deal.js
@@ -17,6 +17,7 @@
  */
 App.DealRoute = Ember.Route.extend(Ember.SimpleAuth.AuthenticatedRouteMixin, {
     model: function(params) {
+    	this.store.unloadAll('deal');
         return this.store.find('deal', params.deal_id);
     },
     afterModel: function(model) {
