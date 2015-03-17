3c8aaa2a89be0a23aac7ead894661c99d9f2dda0
nayzi <ysn.hamzaoui@gmail.com>
2015-03-17 13:49:01 +0100

    cc

--- src/js/app/components/bdc-table.js
+++ src/js/app/components/bdc-table.js
@@ -796,7 +796,7 @@
                 this.set('orderPiece_' + newOrderPiece.get('id'), null);
             } else {
                 newOrderPiece = existingOrderPiece;
-                ffffffffffff
+                
                 if(pieceTypeId==0+'') this.set('orderPiece_' + newOrderPiece.get('id'), 30);
                 this.set('orderPiece_' + newOrderPiece.get('id'), existingOrderPiece.get('piece'));
             }
@@ -846,7 +846,7 @@
             }
 
             this.set('headerData', headerData);
-        },getPiece:function(piece){return this.get('conveyorType.pieceAvailabilities').findBy('PieceId',piece).piece;},
+        },getPiece:function(piece){return this.get('conveyorType.pieceAvailabilities').findBy('Piece.id',piece).piece;},
         initHeaderRowData: function() {
             var headerRowData = [
                 Ember.Object.create({
