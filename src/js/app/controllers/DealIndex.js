37769bb61791da7b33770b90b7630f61606ac869
nayzi <ysn.hamzaoui@gmail.com>
2015-04-15 16:18:21 +0200

    udpate order

--- src/js/app/controllers/DealIndex.js
+++ src/js/app/controllers/DealIndex.js
@@ -21,7 +21,7 @@ App.DealIndexController = Ember.ObjectController.extend({
     actions: {
         showOrder: function(orderId) {
             console.log("dealindex : showorder :" + orderId);
-            this.transitionToRoute("deal.updateOrder")
+            this.transitionToRoute("deal.updateOrder",orderId,3)
         },
         redirect: function(route, id) {
             this.transitionToRoute(route, id);
